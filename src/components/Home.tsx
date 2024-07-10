16900b0471fc561e247447a22ee6772c0d5bf799
Aayush Gajera <aayushg.rorix@gmail.com>
2024-07-10 15:57:07 +0530

    Complete all Add and Delete functionality, Design Remaining

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import './Home.css'
-import { useGetStructureQuery } from '../slices/StructureSlice'
+import { useGetStructureQuery,useAddRootFolderMutation, useAddFileOrFolderFolderMutation ,useDeleteFolderOrFileMutation } from '../slices/StructureSlice'
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { Structure } from '../models/structure';
@@ -9,6 +9,18 @@ import { useEffect, useState } from 'react';
 function home() {
 
   const [structures, setStructures] = useState<Structure[]>([])
+  const [addRootFolder , {isLoading: addRootFolderLoading}] = useAddRootFolderMutation();
+  const [showRootAddBox, setShowRootAddBox] = useState(false)
+  const [rootName, setRootName] = useState('')
+
+  const [showOptions, setShowOptions] = useState(false)
+  const [addType, setAddType] = useState('')
+  const [showAddBox, setShowAddBox] = useState(false)
+  const [elementName, setElementName] = useState('')
+  const [currentElementId, setCurrentElementId] = useState('')
+
+  const [addFileOrFolderFolder,  {isLoading: addFileOrFolderFolderLoading}] = useAddFileOrFolderFolderMutation();
+  const [deleteFolderOrFile,  {isLoading: deleteFolderOrFileLoading}] = useDeleteFolderOrFileMutation();
 
   interface QueryState {
       data?: Structure[];
@@ -16,33 +28,75 @@ function home() {
 
   const { isLoading: getStructureLoading } = useGetStructureQuery();
 
-  // const allStructure = useSelector((state:RootState) => {
-  //   const queries = Object.values(state.api.queries) as QueryState[];
+  const allStructure = useSelector((state:RootState) => {
+    const queries = Object.values(state.api.queries) as QueryState[];
 
-  //   return queries.length > 0 && queries[0].data ? queries[0].data : [];
-  // });
+    return queries.length > 0 && queries[0].data ? queries[0].data : [];
+  });
 
-  const allStructure = [{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[{"_id":"66619cb52cc341a3175e5b71","value":"root","type":"folder","children":[{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[]},{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[]},{"_id":"66619cc42cc341a3175e5b77","value":"top","type":"folder","children":[]}]}]},
-                        {"_id":"6667e4e8cea8019692bac75c","value":"string","type":"folder","children":[{"_id":"667028c10f5296f8b8c871b4","value":"ss","type":"folder","children":[{"_id":"667028c90f5296f8b8c871e3","value":"dd","type":"file","children":[]},{"_id":"667028d40f5296f8b8c87214","value":"dss","type":"folder","children":[{"_id":"667028e00f5296f8b8c87247","value":"hjj","type":"folder","children":[]}]}]}]},
-                        {"_id":"667028b30f5296f8b8c8706e","value":"ddd","type":"folder","children":[{"_id":"667028c10f5296f8b8c871b4","value":"ss","type":"folder","children":[{"_id":"667028c90f5296f8b8c871e3","value":"dd","type":"file","children":[]},{"_id":"667028d40f5296f8b8c87214","value":"dss","type":"folder","children":[{"_id":"667028e00f5296f8b8c87247","value":"hjj","type":"folder","children":[]}]}]}]}
-                      ]
+  // const allStructure = [{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[{"_id":"66619cb52cc341a3175e5b71","value":"root","type":"folder","children":[{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[]},{"_id":"666199542cc341a3175e5b6c","value":"home","type":"folder","children":[]},{"_id":"66619cc42cc341a3175e5b77","value":"top","type":"folder","children":[]}]}]},
+  //                       {"_id":"6667e4e8cea8019692bac75c","value":"string","type":"folder","children":[{"_id":"667028c10f5296f8b8c871b4","value":"ss","type":"folder","children":[{"_id":"667028c90f5296f8b8c871e3","value":"dd","type":"file","children":[]},{"_id":"667028d40f5296f8b8c87214","value":"dss","type":"folder","children":[{"_id":"667028e00f5296f8b8c87247","value":"hjj","type":"folder","children":[]}]}]}]},
+  //                       {"_id":"667028b30f5296f8b8c8706e","value":"ddd","type":"folder","children":[{"_id":"667028c10f5296f8b8c871b4","value":"ss","type":"folder","children":[{"_id":"667028c90f5296f8b8c871e3","value":"dd","type":"file","children":[]},{"_id":"667028d40f5296f8b8c87214","value":"dss","type":"folder","children":[{"_id":"667028e00f5296f8b8c87247","value":"hjj","type":"folder","children":[]}]}]}]}
+  //                     ]
   
   useEffect(()=>{
     setStructures(allStructure)
     console.log('====>',structures)
   },[getStructureLoading])
 
+  function deleteElement(id:string){
+    deleteFolderOrFile(id)
+  }
+
+  function addElement(id:string){
+    setCurrentElementId(id)
+    setShowOptions(true)
+    // setShowAddBox(true)
+  }
+
+  function selectElement(type:string){
+    setAddType(type)
+    setShowOptions(false)
+    setShowAddBox(true)
+  }
+
+  function addRight(parent_id:string){
+    addFileOrFolderFolder({
+      value:elementName,
+      type:addType,
+      parent:parent_id,
+    })
+    setShowOptions(false)
+    setShowAddBox(false)
+    setElementName('')
+    setCurrentElementId('')
+  }
+
+  function addWrong(){
+    // setShowOptions(false)
+    setShowAddBox(false)
+    setElementName('')
+    setCurrentElementId('')
+  }
+
+  function handleOnChange(e:React.ChangeEvent<HTMLInputElement>){
+    setElementName(e.target.value)
+  }
+
+
   function getFolder(structure:Structure){
     return(
       <>
         <li key={structure._id} className={`${structure.type} tree-lines `}>
           <p className='list-tree'>
-            <img src={structure.type==='folder'?'public/folder.png':'public/file.png'} className='image'></img>
-            <span className='fname'>
-              {structure.value}
-              {structure.type==='folder' && <span className='plus'>+</span>}
-              {<span className='minus'>-</span>}
-            </span>
+            <div>
+              <img src={structure.type==='folder'?'public/folder.png':'public/file.png'} className='image'></img>
+              <span className='fname'>
+                {structure.value}
+                {structure.type==='folder' && <span className='plus' onClick={()=>addElement(structure._id)}>+</span>}
+                {<span className='minus' onClick={() => deleteElement(structure._id)}>-</span>}
+              </span>
+            </div>
           </p>
           {structure.children.length > 0 && (
             <div className="folder-strucrure">
@@ -51,6 +105,26 @@ function home() {
               ))}
             </div>
           )}
+
+          <div>
+            {showOptions && currentElementId===structure._id &&
+              <div className='list-tree'>
+                <button onClick={() => selectElement("file")}>File</button>
+                <button onClick={() => selectElement("folder")} >Folder</button>
+              </div>
+            }
+            {showAddBox && currentElementId===structure._id && 
+              <div className='new-root-node list-tree'>
+                <img src={addType==='folder'?'public/folder.png':'public/file.png'} className='image'></img>
+                <form>
+                  <input type='text' name="rootName" value={elementName} onChange={handleOnChange}/>
+                  <button className="add-btn" onClick={() => addRight(structure._id)}>✔</button>
+                  <button className="cancel-btn" onClick={addWrong}>✕</button>
+                </form>
+              </div>
+            }
+          </div>
+          
         </li>
       </>
     )
@@ -65,7 +139,6 @@ function home() {
         )
       )}
     </>)
-    
   }
 
   useEffect(()=>{
@@ -73,16 +146,46 @@ function home() {
     console.log("===>",structures)
   },[getStructureLoading])
 
+  function clickAddRootFolder(){
+    setShowRootAddBox(true)
+  }
+
+  function handleRootOnChange(e: React.ChangeEvent<HTMLInputElement>){
+    setRootName(e.target.value)
+  }
+
+  function addRootRight(){
+    if(rootName){
+      setShowRootAddBox(false)
+      addRootFolder({value:rootName})
+    }
+    setRootName('')
+  }
+
+  function addRootWrong(){
+    setShowRootAddBox(false)
+    setRootName('')
+  }
+
   return (
     <div>
         <h1  className="heading">FOLDER STRUCTURE</h1>
 
-        <button type='button' className='add-folder'>Add folder to root</button>
-        <div>
-          {/* <ul className='folder-strucrure'> */}
+        <button type='button' className='add-folder' onClick={clickAddRootFolder}>Add folder to root</button>
+
+        {(addRootFolderLoading || deleteFolderOrFileLoading || addFileOrFolderFolderLoading || getStructureLoading) && (<img className="img-loader" src="public\ripples.svg" alt="Loading..." />)}
+        {!addRootFolderLoading && <div>
             {getStructure(allStructure)}
-          {/* </ul> */}
-        </div>
+        </div>}
+
+        {showRootAddBox && <div className='new-root-node'>
+          <img src='public/folder.png' className='image'></img>
+          <form>
+            <input type='text' name="rootName" value={rootName} onChange={handleRootOnChange}/>
+            <button className="add-btn" onClick={() => addRootRight()}>✔</button>
+            <button className="cancel-btn" onClick={addRootWrong}>✕</button>
+          </form>
+        </div>}
     </div>
   )
 }
--- src/slices/StructureSlice.tsx
+++ src/slices/StructureSlice.tsx
@@ -1,6 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Structure } from "../models/structure";
 
+interface RootFolder{
+    value: string
+}
+
+interface FileOrFolder{
+    value: string;
+    type: string;
+    parent: string;
+}
+
 export const structureApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://folder-structure-api.onrender.com/',
@@ -10,6 +20,32 @@ export const structureApi = createApi({
         getStructure: builder.query<Structure, void>({
             query: () => '',
             providesTags: ['Structure'],
+        }),
+
+        addRootFolder: builder.mutation<void, RootFolder>({
+            query: (structure) => ({
+                url: '/initialize-root',
+                method: 'POST',
+                body: structure,
+            }),
+            invalidatesTags: ["Structure"],
+        }),
+
+        addFileOrFolderFolder: builder.mutation<void, FileOrFolder>({
+            query: (structure) => ({
+                url: '',
+                method: 'POST',
+                body: structure,
+            }),
+            invalidatesTags: ["Structure"],
+        }),
+
+        deleteFolderOrFile: builder.mutation<void, string>({
+            query: (id) => ({
+                url: `/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ["Structure"],
         }),
     })
 });
@@ -25,6 +61,33 @@ export type UseGetStructureQuery = typeof structureApi extends {
     };
 } ? T : never;
 
+export type useAddRootFolderMutation = typeof structureApi extends {
+    endpoints: {
+        addRootFolder: {
+            useQuery: infer T;
+        };
+    };
+} ? T : any;
+
+export type useAddFileOrFolderFolderMutation = typeof structureApi extends {
+    endpoints: {
+        addFileOrFolderFolder: {
+            useQuery: infer T;
+        };
+    };
+} ? T : any;
+
+export type useDeleteFolderOrFileMutation = typeof structureApi extends {
+    endpoints: {
+        deleteFolderOrFile: {
+            useQuery: infer T;
+        };
+    };
+} ? T : any;
+
 // Export the useGetStructureQuery hook with the defined type
 export const useGetStructureQuery: UseGetStructureQuery = structureApi.endpoints.getStructure.useQuery;
-// export const { useGetStructureQuery } = structureApi;
+export const useAddRootFolderMutation: useAddRootFolderMutation = structureApi.endpoints.addRootFolder.useMutation;
+export const useAddFileOrFolderFolderMutation: useAddFileOrFolderFolderMutation = structureApi.endpoints.addFileOrFolderFolder.useMutation;
+export const useDeleteFolderOrFileMutation: useDeleteFolderOrFileMutation = structureApi.endpoints.deleteFolderOrFile.useMutation;
+// export const { useAddRootFolderMutation } = structureApi;
